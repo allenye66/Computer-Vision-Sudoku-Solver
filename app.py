6ef1e11a7292c434dbaf846bb129a105635892f8
Allen <allenye66@gmail.com>
2020-05-12 23:49:33 -0700

    framework

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import solve
 
 app = Flask(__name__)
 
@@ -8,10 +8,20 @@ def index():
 	print("received the values")
 	return render_template('index.html')
 
-@app.route('/show', methods = ['GET' ,'POST'])
-def show():
+@app.route('/obtainInput', methods = ['GET' ,'POST'])
+def obtainInput():
+
+
+	w, h = 9, 9;
+	grid = [[0 for x in range(w)] for y in range(h)] 
+
+	#determine the numbers and send them to another python file to process the numbers and solve the grid
+
+
+	answers = solve(grid)
+
 	
-	return render_template('pass.html', txt = "asdf", wordbank = "asdf")
+	return render_template('pass.html', grid)
 
 if __name__ == '__main__':
 	app.run(debug=True)
