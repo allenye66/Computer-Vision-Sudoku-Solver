88f757d0785c6c72c6275bcbce2b9038f70118f3
Allen <allenye66@gmail.com>
2020-05-15 10:27:27 -0700

    user input
    users can only input a single number inside the grid

---
+++ static/script.js
@@ -0,0 +1,22 @@
+// Restricts input for each element in the set of matched elements to the given inputFilter.
+(function($) {
+  $.fn.inputFilter = function(inputFilter) {
+    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  };
+}(jQuery));
+
+
+// Install input filters.
+$("#intTextBox").inputFilter(function(value) {
+  return /^-?\d*$/.test(value); });
