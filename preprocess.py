246291150278b0dbfa54c72791e94664de149613
Allen <allenye66@gmail.com>
2020-05-22 18:04:26 -0700

    adaptive thresholding

--- preprocess.py
+++ preprocess.py
@@ -1,22 +1,17 @@
-import cv2
-import numpy as np
+import cv2  
+import numpy as np  
 from matplotlib import pyplot as plt
-def process(input):
-	img = cv2.imread(input,0)
-	img = cv2.medianBlur(img,5)
 
-	ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-	th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,11,2)
-	th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,11,2)
+   
 
-	titles = ['Original Image', 'Global Thresholding (v = 127)',
-            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-	images = [img, th1, th2, th3]
+image1 = cv2.imread('/kaggle/input/sudoku-original.jpg')  
+img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY) 
 
-	for i in xrange(4):
-    	plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
-    	plt.title(titles[i])
-    	plt.xticks([]),plt.yticks([])
-	plt.show()
+thresh1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, 
+                                          cv2.THRESH_BINARY, 199, 5) 
+  
+thresh2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+                                          cv2.THRESH_BINARY, 199, 5)  
+plt.imshow(thresh1) 
+plt.imshow(thresh2) 
+
