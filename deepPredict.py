74e598cda478801c3d2c4487e3179bec146d8466
Allen <allenye66@gmail.com>
2020-05-26 13:59:50 -0700

    fixed predictions from CNN
    turns out the MNIST dataset has all its data with inverted colors, so I had to invert all the data I passed into the trained model

--- deepPredict.py
+++ deepPredict.py
@@ -1,16 +1,23 @@
 from keras.models import load_model
 from keras.datasets import mnist
-
 import matplotlib.pyplot as plt
+    
+import numpy
+
+
 
 model = load_model('digits.h5')
 import cv2
 #model.summary()
 
+
 def prepare(filepath):
 	img_size = 28
 	img_arr = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
 	new_arr = cv2.resize(img_arr, (img_size, img_size))
+	new_arr = numpy.invert(new_arr)
+	#plt.imshow(new_arr)
+	#plt.show()
 	return new_arr.reshape(-1, img_size, img_size, 1)
 
 
@@ -20,6 +27,7 @@ def prepare(filepath):
 #plt.show()
 img = X_train[1]
 img = img.reshape(-1, 28, 28, 1)
+
 
 
 prediction = model.predict_classes([prepare("/Users/allen/Desktop/one.png")])
